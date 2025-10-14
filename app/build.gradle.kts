f3a9d49d2615c5748ea69b2edb006b60813d6fcc
MarianoCR124 <mariano_cr@tesch.edu.mx>
2025-10-14 16:13:12 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
+//mariano contreras esta presente 
 
 android {
     namespace = "com.example.retrofit2025"
